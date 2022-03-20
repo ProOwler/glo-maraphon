0f626b040a69724b02f75a9c1f3fc7dcc0aca316
ProOwler <proowler@gmail.com>
2022-03-21 02:01:52 +0700

    Урок 1, 35 минут

--- js/tabs.js
+++ js/tabs.js
@@ -1,1 +1,27 @@
-console.log(document.getElementById('main'));
+//const main = document.getElementById('main');
+/*
+tabButton.onclick = () => {
+    console.log('click');
+}
+*/
+const tabButtons = document.querySelectorAll('.design-list__item');
+const tabDescriptions = document.querySelectorAll('.design-text .design__descr');
+const tabImages = document.querySelectorAll('.design-images');
+
+tabButtons.forEach((tabButton, index) => {
+    tabButton.addEventListener('click', (event) => {
+        tabButtons.forEach((btn) => {
+            btn.classList.toggle('design-list__item_active');
+        });
+        tabDescriptions.forEach((descr) => {
+            descr.classList.toggle('hidden');
+        });
+        tabImages.forEach((imagesBlock, indexImagesBlocks) => {
+            if (index === indexImagesBlocks) {
+                imagesBlock.classList.remove('hidden');
+            } else {
+                imagesBlock.classList.add('hidden');
+            }
+        });
+    });
+});
